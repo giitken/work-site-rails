74ccfa681159154e1deaf5ed9d02e70dbf419951
ken <kirie115@gmail.com>
2019-01-10 19:30:52 +0900

    cp7

--- taskleaf/app/controllers/tasks_controller.rb
+++ taskleaf/app/controllers/tasks_controller.rb
@@ -2,7 +2,13 @@ class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
     @q = current_user.tasks.ransack(params[:q])
-    @tasks = @q.result(distinct: true)
+    @tasks = @q.result(distinct: true).page(params[:page])
+
+    respond_to do | format |
+      format.html
+      format.csv {
+        send_data @tasks.generate_csv, filename: "tasks-#{Time.zone.now.strftime('%Y%m%d%S')}.csv"}
+    end
   end
 
   def show
@@ -24,6 +30,9 @@ class TasksController < ApplicationController
     end
 
     if @task.save
+      TaskMailer.creation_email(@task).deliver_now
+      SampleJob.perform_later
+      logger.debug "task: #{@task.attributes.inspect}"
       redirect_to @task, notice: "タスク「#{@task.name}」を登録しました。"
     else
       render :new
@@ -45,10 +54,15 @@ class TasksController < ApplicationController
     render :new unless @task.valid?
   end
 
+  def import
+    current_user.tasks.import(params[:file])
+    redirect_to tasks_url, notice: "タスクを追加しました。"
+  end
+
   private
 
   def task_params
-    params.require(:task).permit(:name, :description)
+    params.require(:task).permit(:name, :description, :image)
   end
 
   def set_task
